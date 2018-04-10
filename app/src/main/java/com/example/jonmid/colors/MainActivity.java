5a75de2540d9574fec1e8529ef02b21a4456eb84
jhonnybenavidesm <jhonny1290bena@gmail.com>
2018-04-10 17:03:34 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jonmid/colors/MainActivity.java
@@ -0,0 +1,124 @@
+package com.example.jonmid.colors;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+
+    int seekR, seekB,seekG,seekA,NumberSeekR,NumberseekG,NumberseekB,NumberseekA;
+    SeekBar seekBarRed,seekBarGreen,seekBarBlue,seekBarAlpha;
+    View viewColor;
+    TextView NumberText;
+    TextView NumberText2;
+    TextView NumberText3;
+    TextView NumberText4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+        seekBarRed=(SeekBar) findViewById(R.id.seek_red);
+        seekBarBlue=(SeekBar) findViewById(R.id.seek_blue);
+        seekBarGreen=(SeekBar) findViewById(R.id.seek_green);
+        seekBarAlpha=(SeekBar) findViewById(R.id.seek_Alpha);
+        viewColor = (View) findViewById(R.id.view);
+        NumberText = (TextView) findViewById(R.id.seekNumber);
+        NumberText2 = (TextView) findViewById(R.id.seekNumber2);
+        NumberText3 = (TextView) findViewById(R.id.seekNumber3);
+        NumberText4 = (TextView) findViewById(R.id.seekNumber4);
+
+        seekBarRed.setOnSeekBarChangeListener(this);
+        seekBarBlue.setOnSeekBarChangeListener(this);
+        seekBarGreen.setOnSeekBarChangeListener(this);
+        seekBarAlpha.setOnSeekBarChangeListener(this);
+
+        //registerForContextMenu(textViewName);
+
+    }
+
+    public void UpdateBackground(){
+
+        seekR = seekBarRed.getProgress();
+        seekB = seekBarBlue.getProgress();
+        seekG = seekBarGreen.getProgress();
+        seekA = seekBarAlpha.getProgress();
+
+        int color = Color.argb(seekR,seekG,seekB,seekA);
+        viewColor.setBackgroundColor(color);
+    }
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.index_menu, menu);
+        return true;
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.id_menu_white:
+                Toast.makeText(this, "item 1", Toast.LENGTH_SHORT).show();
+
+                return true;
+            case R.id.id_menu_black:
+                Toast.makeText(this, "item 2", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.index_menu, menu);
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+
+        UpdateBackground();
+        NumberSeekR=seekBarRed.getProgress();
+        NumberText.setText(String.valueOf(NumberSeekR));
+        NumberseekG=seekBarGreen.getProgress();
+        NumberText2.setText(String.valueOf(NumberseekG));
+        NumberseekB=seekBarBlue.getProgress();
+        NumberText3.setText(String.valueOf(NumberseekB));
+        NumberseekA=seekBarAlpha.getProgress();
+        NumberText4.setText(String.valueOf(NumberseekA));
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+}
